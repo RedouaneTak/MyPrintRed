60b9532d3d62d89bdaa50a84f62c9bcd6564f59b
Redouane <redouane.takdjerad@gmail.com>
2023-03-30 15:35:48 +0200

    OptionLigneCommande

---
+++ src/main/java/fr/rt/MyPrintRed/entities/OptionLigneCommande.java
@@ -0,0 +1,21 @@
+package fr.rt.MyPrintRed.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "option_ligne_commande", schema = "public", catalog = "MYPRINTRED")
+public class OptionLigneCommande {
+
+    @EmbeddedId
+    private OptionLigneCommandePK optionLigneCommandePK;
+}
